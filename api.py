8e3382b5ad67ff04214f8b7c1bd344c34f069b3b
moontr3 <rodionkovalkovskii@gmail.com>
2024-09-12 01:58:49 +0300

    init

---
+++ api.py
@@ -0,0 +1,227 @@
+import random
+from typing import *
+from config import *
+import json
+import os
+import time
+from log import *
+from discord.ext import commands
+
+ 
+# user and user-related classes
+
+class User:
+    def __init__(self, id:int, data:dict={}):
+        '''
+        Represents a user.
+        '''
+        self.id: int = id
+        
+        self.saved_message: "str | None" = data.get('saved_message', None)
+
+    
+    def to_dict(self) -> dict:
+        '''
+        Converts the class to a dictionary to store in the file.
+        '''
+        return {
+            "saved_message": self.saved_message
+        }
+    
+
+class Whisper:
+    def __init__(self, id:int, data:dict):
+        '''
+        Represents a whisper.
+        '''
+        self.id: int = id
+        self.text: str = data['text']
+        self.owner: int = data['owner']
+        self.viewer: int = data['viewer']
+        self.once: bool = data['once']
+
+    
+    def to_dict(self) -> dict:
+        '''
+        Converts the class to a dictionary to store in the file.
+        '''
+        return {
+            "text": self.text,
+            "owner": self.owner,
+            "viewer": self.viewer,
+            "once": self.once
+        }
+
+
+# manager
+
+class Manager:
+    def __init__(self, users_file:str):
+        '''
+        API and backend manager.
+        '''
+        self.users_file: str = users_file
+        self.whispers: Dict[int, str] = {}
+
+        self.reload()
+        self.reload_data()
+
+
+    def new(self):
+        '''
+        Rewrites the old database with the new one.
+        '''
+        self.users: Dict[int, User] = {}
+
+        self.commit()
+
+
+    def panic(self):
+        '''
+        Creates a duplicate of the database and creates a new one.
+        '''
+        log('Panic!', 'api', WARNING)
+
+        # copying file
+        if os.path.exists(self.users_file):
+            os.rename(self.users_file, self.users_file+'.bak')
+            log(f'Cloned user data file to {self.users_file}.bak', 'api')
+
+        # creating a new one
+        self.new()
+
+
+    def reload(self):
+        '''
+        Reloads user data and bot data.
+        '''
+        # user data
+        try:
+            with open(self.users_file, encoding='utf-8') as f:
+                data = json.load(f)
+        except:
+            self.panic()
+            return
+
+        self.users = {int(id): User(int(id), data) for id, data in data['users'].items()}
+        self.whispers = {int(id): Whisper(int(id), data) for id, data in data['whispers'].items()}
+
+        # saving
+        self.commit()
+
+
+    def reload_data(self):
+        '''
+        Reloads game data.
+        '''
+        # data
+        try:
+            with open(self.users_file, encoding='utf-8') as f:
+                self.data = json.load(f)
+        except Exception as e:
+            log(f'Failed to load data file: {e}', 'api', ERROR)
+            self.data = {}
+
+
+    def commit(self):
+        '''
+        Saves user data to the file.
+        '''
+        data = {
+            'users': {},
+            'whispers': {}
+        }
+
+        # users
+        for i in self.users:
+            data['users'][i] = self.users[i].to_dict()
+
+        # whispers
+        for i in self.whispers:
+            data['whispers'][i] = self.whispers[i].to_dict()
+
+        # saving
+        with open(self.users_file, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False)
+
+
+    def check_user(self, id:int):
+        '''
+        Checks if user exists in database. If not, creates one.
+        '''
+        if id in self.users:
+            return
+        
+        self.users[id] = User(id)
+
+
+    def get_user(self, id:int) -> User:
+        '''
+        Returns user by ID.
+
+        Automatically checks the user.
+        '''
+        self.check_user(id)
+        return self.users[id]
+
+
+    def get_whisper(self, id:int) -> "Whisper | None":
+        '''
+        Returns whisper by message ID.
+
+        Returns None if not found.
+        '''
+        if id not in self.whispers:
+            return None
+        
+        return self.whispers[id]
+    
+
+    def save_whisper(self, id:int, text:str):
+        '''
+        Saves a whisper to a user.
+        '''
+        user = self.get_user(id)
+
+        user.saved_message = text
+
+        self.commit()
+    
+
+    def unsave_whisper(self, id:int):
+        '''
+        Removes a saved whisper.
+        '''
+        user = self.get_user(id)
+
+        user.saved_message = None
+
+        self.commit()
+
+
+    def send_whisper(self, message:int, owner:int, viewer:int, text:int, once:bool):
+        '''
+        Adds a whisper to the database.
+        '''
+        self.whispers[message] = Whisper(
+            message, {
+                "text": text,
+                "owner": owner,
+                "viewer": viewer,
+                "once": once
+            }
+        )
+
+        self.commit()
+
+
+    def remove_whisper(self, id:int):
+        '''
+        Removes a whisper from the database.
+        '''
+        if id not in self.whispers:
+            return
+        
+        self.whispers.pop(id)
+
+        self.commit()
---
+++ main.py
@@ -0,0 +1,257 @@
+import api
+from config import *
+from log import *
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+from typing import *
+import time
+
+# loading token
+load_dotenv()
+TOKEN = os.getenv('BOT_TOKEN')
+
+bot = commands.Bot(command_prefix=PREFIX, intents=discord.Intents.all(), help_command=None)
+mg = api.Manager(USERS_FILE)
+
+# connection events
+
+@bot.event
+async def on_ready():
+    log(f'Ready as {bot.user.name}!')
+
+    # commands = await bot.tree.sync()
+    # log(f'Synced tree with {len(commands)} commands', level=SUCCESS)
+
+
+# events
+
+@bot.event
+async def on_interaction(inter:discord.Interaction):
+    '''
+    Gets called when a button is pressed or a command is used.
+    '''
+    if inter.type == discord.InteractionType.application_command:
+        return
+    
+    # answering
+    log(f'{inter.user.id} pressed on {inter.id}')
+
+    message_id = int(inter.data['custom_id'])
+    whisper = mg.get_whisper(message_id)
+
+    # no whisper
+    if whisper == None:
+        embed = discord.Embed(
+            color=discord.Color.red(),
+            description='**Whisper not found!**\n\n'\
+                'If it was a one-time whisper, it probably expired.'
+        )
+
+    # not owner
+    elif inter.user.id not in [whisper.owner, whisper.viewer]:
+        embed = discord.Embed(
+            color=discord.Color.red(),
+            description='**You are not meant to view this whisper!**'
+        )
+
+    # showing whisper
+    else:
+        title = '🔥 One-time whisper!' if whisper.once else None
+
+        embed = discord.Embed(
+            color=discord.Color.blurple(),
+            description=whisper.text
+        )
+        if title != None:
+            embed.set_author(name=title)
+
+        # removing one-time whispers
+        if inter.user.id == whisper.viewer and whisper.once:
+            print(f'Removing one-time whisper {message_id}')
+            mg.remove_whisper(message_id)
+
+    await inter.response.send_message(embed=embed, ephemeral=True)
+
+
+# commands
+
+save_text_cmd = 'save-text:1283547176848326771'
+
+# text_gr = discord.app_commands.Group(
+#     name='text',
+#     description='Manage currently saved text.'
+# )
+
+@bot.tree.command(
+    name='view-text',
+    description='View your currently saved text.'
+)
+@discord.app_commands.user_install()
+async def view_text(
+    inter:discord.Interaction
+):
+    '''
+    Shows your current saved text.
+    '''
+    user = mg.get_user(inter.user.id)
+
+    if user.saved_message == None:
+        embed = discord.Embed(
+            color=discord.Color.red(),
+            description='**No saved text!**\n\n'\
+                f'Use </{save_text_cmd}> to save your text.'
+        )
+    else:
+        embed = discord.Embed(
+            color=discord.Color.blurple(),
+            description="**Saved text:**\n\n"+user.saved_message
+        )
+
+    await inter.response.send_message(embed=embed,ephemeral=True)
+        
+
+
+@bot.tree.command(
+    name='save-text',
+    description='Saves your text to whisper to someone.'
+)
+@discord.app_commands.user_install()
+@discord.app_commands.describe(
+    text='Your text to whisper to someone',
+)
+async def save_text(
+    inter:discord.Interaction,
+    text:str
+):
+    '''
+    Saves text to whisper.
+    '''
+    if len(text) > 1024:
+        embed = discord.Embed(
+            color=discord.Color.red(),
+            description="Your text must not be longer than 1024 characters!"
+        )
+
+    else:
+        mg.save_whisper(inter.user.id, text)
+
+        embed = discord.Embed(
+            color=discord.Color.blurple(),
+            description="**Success!**"
+        )
+
+    await inter.response.send_message(embed=embed,ephemeral=True)
+
+
+
+@bot.tree.command(
+    name='remove-text',
+    description='Remove saved text.'
+)
+@discord.app_commands.user_install()
+async def remove_text(
+    inter:discord.Interaction
+):
+    '''
+    Removes saved text.
+    '''
+    user = mg.get_user(inter.user.id)
+
+    if user.saved_message == None:
+        embed = discord.Embed(
+            color=discord.Color.red(),
+            description="**You don't have any text saved!**"
+        )
+
+    else:
+        mg.unsave_whisper(inter.user.id)
+
+        embed = discord.Embed(
+            color=discord.Color.blurple(),
+            description="**Text removed!**"
+        )
+
+    await inter.response.send_message(embed=embed,ephemeral=True)
+
+
+@bot.tree.command(
+    name='whisper',
+    description='Whisper saved text to someone.'
+)
+@discord.app_commands.describe(
+    user='User to whisper saved text to',
+    selfdestruct='Whether the whisper should only be viewed once (no by default)',
+)
+@discord.app_commands.user_install()
+async def whisper(
+    inter:discord.Interaction,
+    user:discord.User,
+    selfdestruct:Literal[   
+        'Yes (viewable only once)',
+        'No (viewable anytime)'
+    ]='No (viewable anytime)'
+):
+    '''
+    Whispers saved text.
+    '''
+    bot_user = mg.get_user(inter.user.id)
+    
+    if user.id == inter.user.id:
+        embed = discord.Embed(
+            color=discord.Color.red(),
+            description="**You can't whisper to yourself!**"
+        )
+        await inter.response.send_message(embed=embed,ephemeral=True)
+        return
+
+    if bot_user.saved_message == None:
+        embed = discord.Embed(
+            color=discord.Color.red(),
+            description="**You don't have any text saved!**\n\n"\
+                f'Use </{save_text_cmd}> to save your text and try again.'
+        )
+        await inter.response.send_message(embed=embed,ephemeral=True)
+        return
+        
+    # sending
+    selfdestruct = selfdestruct == 'Yes (viewable only once)'
+
+    embed = discord.Embed(
+        description=f'{inter.user.mention} whispers to {user.mention}...'\
+            f'\n\nClick on the button to read the whisper.'
+    )
+
+    view = discord.ui.View()
+
+    button = discord.ui.Button(
+        style=discord.ButtonStyle.blurple,
+        label='Loading...', disabled=True
+    )
+    view.add_item(button)
+
+    await inter.response.send_message(embed=embed, view=view)
+    original = await inter.original_response()
+
+    mg.send_whisper(
+        original.id, inter.user.id,
+        user.id, bot_user.saved_message,
+        selfdestruct
+    )
+
+    # new view
+    view = discord.ui.View()
+
+    button = discord.ui.Button(
+        style=discord.ButtonStyle.blurple,
+        label='Read whisper', custom_id=str(original.id)
+    )
+    view.add_item(button)
+
+    await inter.edit_original_response(view=view)
+
+
+## RUNNING BOT
+bot.run(TOKEN)
